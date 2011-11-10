443490c2ced5fc0f89b2b9b74b3c3a8ca902f101
Trent Kocurek <t2kocurek@gmail.com>
2011-11-09 23:54:01 -0600

    still just messing around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def init
+    @search = Twitter::Search.new
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,15 @@ class PagesController < ApplicationController
   before_filter :set_globals
 
   def set_globals
-    @search = Twitter::Search.new
+    @search ||= Twitter::Search.new
+    @tweets ||= ping_twitter_for_tweets_with("dev")
   end
 
   def index
-    @tweets = @search.hashtag("dev").language("en").no_retweets.fetch
+    @tweets
+  end
+  
+  def ping_twitter_for_tweets_with(query)
+    @tweets = @search.hashtag(query).language("en").no_retweets.per_page(20).fetch
   end
 end
